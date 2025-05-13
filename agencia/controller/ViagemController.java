d09efff3cfe4a69533afc6b9ea821a50217614a7
Leonardo Toledo <leonardo.programador20@hotmail.com>
2025-05-12 23:18:28 -0400

    atualizaçao das page nao colocadas

--- agencia/controller/ViagemController.java
+++ agencia/controller/ViagemController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -40,6 +41,10 @@ public class ViagemController {
                      .orElse(ResponseEntity.notFound().build());
     }
 
+    @GetMapping("/{page}/{size}")
+    public Page<Viagem> listarviagens(@PathVariable int page, @PathVariable int size){
+        return this.viagemService.listarViagensPaginados(page, size);
+    }
     @PostMapping
     public ResponseEntity<Viagem> criar(@RequestBody Viagem viagem) {
         Viagem nova = viagemService.add(viagem);
